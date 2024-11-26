f8d03d8563c1b20e5483c98ab9d8099d50594a96
Abu Talha <53362081+dev-talha@users.noreply.github.com>
2024-11-26 20:27:28 +0600

    Update config.php

--- backend/config.php
+++ backend/config.php
@@ -2,9 +2,9 @@
 
 // Database Configuration
 define('DB_HOST', 'localhost');
-define('DB_USER', 'alislahf_lost_and_found');
-define('DB_PASS', 'lost_and_found@123');
-define('DB_NAME', 'alislahf_lost_and_found');
+define('DB_USER', 'user-name');
+define('DB_PASS', 'password');
+define('DB_NAME', 'databasename');
 
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
